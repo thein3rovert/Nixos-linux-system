d20584accaf972851dcf96479e08e635239c1c50
thein3rovert <danielolaibi@gmail.com>
2025-01-01 11:59:07 +0000

    removing terminal debug logging

--- home-manager/apps/cli/zsh.nix
+++ home-manager/apps/cli/zsh.nix
@@ -25,10 +25,7 @@ in {
         export NIX_PATH="nixpkgs=channel:nixos-unstable"
         export NIX_LOG="info"
         export TERMINAL="kitty"
-        # Debugging output
-        echo "NIX_PATH set to: $NIX_PATH"
-        echo "NIX_LOG set to: $NIX_LOG"
-        echo "TERMINAL set to: $TERMINAL"
+
         # Check if the current TTY is /dev/tty1 and run Hyprland
         if [[ $(tty) == "/dev/tty1" ]]; then
           exec Hyprland &> /dev/null
