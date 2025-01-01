3498f496d2ae49645535bf0b24e713afe083d29b
thein3rovert <danielolaibi@gmail.com>
2025-01-01 14:31:54 +0000

    Make changes to kitty colorscheme

--- home-manager/introvert/home.nix
+++ home-manager/introvert/home.nix
@@ -31,7 +31,7 @@ in {
  # ../common
   ];
 
-  colorScheme = inputs.nix-colors.colorSchemes.tokyo-night-dark;
+  colorScheme = inputs.nix-colors.colorSchemes.tokyo-night-terminal-storm;
 
    programs = {
     kitty = {
@@ -69,7 +69,7 @@ in {
         # add some nice default configs
         repaint_delay = "60";
         sync_to_monitor = "yes";
-        background_opacity = "0.8";
+        background_opacity = "1.0";
         background_blur = "1";
         tab_bar_style = "powerline";
         tab_powerline_style = "round";
